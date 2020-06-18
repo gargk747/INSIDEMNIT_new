bb3a9778ff9d7ca902528d19c2eab8a8bee58007
gargk747 <gargk747@gmail.com>
2020-06-19 03:06:13 +0530

    routes

--- app/src/main/java/com/example/insidemnit/NavigationView.java
+++ app/src/main/java/com/example/insidemnit/NavigationView.java
@@ -152,47 +152,21 @@ public class NavigationView extends FragmentActivity implements OnMapReadyCallba
     private void dijkstra(int[][] adjacencyMatrix, int startVertex, int destinationVertex) {
         int nVertices = adjacencyMatrix[0].length;
 
-        // shortestDistances[i] will hold the
-        // shortest distance from src to i
         int[] shortestDistances = new int[nVertices];
-
-        // added[i] will true if vertex i is
-        // included / in shortest path tree
-        // or shortest distance from src to
-        // i is finalized
         boolean[] added = new boolean[nVertices];
 
-        // Initialize all distances as
-        // INFINITE and added[] as false
         for (int vertexIndex = 0; vertexIndex < nVertices;
              vertexIndex++)
         {
             shortestDistances[vertexIndex] = Integer.MAX_VALUE;
             added[vertexIndex] = false;
         }
-
-        // Distance of source vertex from
-        // itself is always 0
         shortestDistances[startVertex] = 0;
-
-        // Parent array to store shortest
-        // path tree
         int[] parents = new int[nVertices];
-
-        // The starting vertex does not
-        // have a parent
         parents[startVertex] = NO_PARENT;
 
-        // Find shortest path for all
-        // vertices
         for (int i = 0; i < nVertices; i++)
         {
-
-            // Pick the minimum distance vertex
-            // from the set of vertices not yet
-            // processed. nearestVertex is
-            // always equal to startNode in
-            // first iteration.
             int nearestVertex = -1;
             int shortestDistance = Integer.MAX_VALUE;
             for (int vertexIndex = 0;
@@ -207,14 +181,7 @@ public class NavigationView extends FragmentActivity implements OnMapReadyCallba
                     shortestDistance = shortestDistances[vertexIndex];
                 }
             }
-
-            // Mark the picked vertex as
-            // processed
             added[nearestVertex] = true;
-
-            // Update dist value of the
-            // adjacent vertices of the
-            // picked vertex.
             for (int vertexIndex = 0;
                  vertexIndex < nVertices;
                  vertexIndex++)
@@ -234,10 +201,6 @@ public class NavigationView extends FragmentActivity implements OnMapReadyCallba
 
         printSolution(startVertex, shortestDistances, parents,destinationVertex);
     }
-
-    // A utility function to print
-    // the constructed distances
-    // array and shortest paths
     private void printSolution(int startVertex,
                                int[] distances,
                                int[] parents, int destinationVertex)
@@ -254,10 +217,6 @@ public class NavigationView extends FragmentActivity implements OnMapReadyCallba
         }
 
     }
-
-    // Function to print shortest path
-    // from source to currentVertex
-    // using parents array
     private int[] printPath(int startVertex, int currentVertex,
                                   int[] parents)
     {
@@ -265,8 +224,6 @@ public class NavigationView extends FragmentActivity implements OnMapReadyCallba
 
         Log.d("new", "printPath: ");
         Log.d("point", ""+currentVertex);
-        // Base case : Source node has
-        // been processed
         if (currentVertex == NO_PARENT)
         {
             return test;
